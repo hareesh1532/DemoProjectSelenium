6953d198030a1062538b7712a4ef7fb18d25afa2
hareesh1532 <hareeshmecch@gmail.com>
2025-03-04 17:54:06 +0530

    commit

---
+++ HareeshFW/src/test/java/com/comcast/crm/contact/CreateContactTest.java
@@ -0,0 +1,19 @@
+package com.comcast.crm.contact;
+
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest {
+	@Test
+	public void CreateContact() {
+		System.out.println("Create contact");
+
+	}
+
+	@Test
+	public void DeleteContact() {
+		System.out.println("Delete contact");
+
+	}
+
+}
---
+++ HareeshFW/src/test/java/com/comcast/crm/contact/CreateContactWithOrgTest.java
@@ -0,0 +1,20 @@
+package com.comcast.crm.contact;
+
+
+import org.testng.annotations.Test;
+
+public class CreateContactWithOrgTest {
+
+	@Test
+	public void CreateOrg() {
+		System.out.println("Create contact");
+
+	}
+
+	@Test
+	public void DeleteOrg() {
+		System.out.println("Delete contact");
+
+	}
+
+}
